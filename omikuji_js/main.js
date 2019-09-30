ce79178290461dd57c64ccc5f1ef8f663918f979
kaijunna <shell1016221@gmail.com>
2019-10-01 01:53:05 +0900

    no message

--- omikuji_js/main.js
+++ omikuji_js/main.js
@@ -1,1 +1,41 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})
+var currenHour = 11;
+if (currenHour < 12) {
+  console.log("AM");
+}else if (currenHour > 12) {
+  console.log("pm");
+}else {
+  console.log("noon");
+}
+
+var currenHour = 12;
+if (currenHour < 12) {
+  console.log("AM");
+}else if (currenHour > 12) {
+  console.log("pm");
+}else {
+  console.log("noon");
+}
+
+var currenHour = 20;
+if (currenHour < 12) {
+  console.log("AM");
+}else if (currenHour > 12) {
+  console.log("pm");
+}else {
+  console.log("noon");
+}
+
+//↓関数
+
+function ChackAmpm (currentHour) {
+  if (currentHour < 12) {
+    console.log("AM");
+  }else if (ChackAmpm > 12);{
+    console.log("PM");
+  }else 
+    console.log("noon");
+}
+
+checkAmpm (11);
+checkAmpm (12);
+checkAmpm (20);
