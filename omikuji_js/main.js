b5d57ff02b0b4f1e88e48c6ac0704dea8b8ec33b
kaijunna <shell1016221@gmail.com>
2019-10-01 16:07:28 +0900

    no message

--- omikuji_js/main.js
+++ omikuji_js/main.js
@@ -1,41 +1,9 @@
-var currenHour = 11;
-if (currenHour < 12) {
-  console.log("AM");
-}else if (currenHour > 12) {
-  console.log("pm");
-}else {
-  console.log("noon");
-}
-
-var currenHour = 12;
-if (currenHour < 12) {
-  console.log("AM");
-}else if (currenHour > 12) {
-  console.log("pm");
-}else {
-  console.log("noon");
-}
-
-var currenHour = 20;
-if (currenHour < 12) {
-  console.log("AM");
-}else if (currenHour > 12) {
-  console.log("pm");
-}else {
-  console.log("noon");
-}
-
-//↓関数
-
-function ChackAmpm (currentHour) {
-  if (currentHour < 12) {
-    console.log("AM");
-  }else if (ChackAmpm > 12);{
-    console.log("PM");
-  }else 
-    console.log("noon");
-}
-
-checkAmpm (11);
-checkAmpm (12);
-checkAmpm (20);
+var human = {
+      name :'名前',
+      height :'身長',
+      weight :'体重',
+};
+alert(human);
+alert(human.name);
+alert(human.height);
+alert(human.weight);
