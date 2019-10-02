2c1f25c3bd5ae23d0753ac17241c8e6100f85d9c
kaijunna <shell1016221@gmail.com>
2019-10-02 18:26:11 +0900

    no message

--- omikuji_js/main.js
+++ omikuji_js/main.js
@@ -1,6 +1,8 @@
-window.onload =function(){
-  alert("hello");
-}
-window.onclick =function() {
-  alert("clicked");
-}
+var button = document.getElementById("button1");
+button.onclick = function() {
+    Element = document.getElementById("input");
+    var P = document.createElement('p');
+    var Div = document.getElementById("div");
+    P.innerHTML = Element.value;
+    Div.appendChild(P);
+};
