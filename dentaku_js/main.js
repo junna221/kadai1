829b0363467e58fa5a6363ff60353eeb8f4b6163
kaijunna <shell1016221@gmail.com>
2019-10-03 06:23:46 +0900

    no message

---
+++ dentaku_js/main.js
@@ -0,0 +1,7 @@
+function edit(elem) {
+ result.value = result.value + elem.value;
+}
+
+function calc() {
+ result.value = new Function("return " + result.value)();
+}
--- omikuji_js/main.js
+++ omikuji_js/main.js
@@ -1,8 +1,5 @@
-var button = document.getElementById("button1");
-button.onclick = function(){
-  var Element = document.getElementById("input");
-  var P = document.createElement('p');
-  var Div = document.getElementById("div");
-  P.innerHTML = Element.value;
-  Div.appendChild(P);
-};
+$(function(){
+  $('p').click(function(){
+      $(this).text("クリックされました");
+  });
+});
