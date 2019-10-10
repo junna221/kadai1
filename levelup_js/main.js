9e0c5896e7d46a68175386ca2ed3c6e2122fbd74
kaijunna <shell1016221@gmail.com>
2019-10-10 17:05:48 +0900

    no message

--- levelup_js/main.js
+++ levelup_js/main.js
@@ -1,14 +1,22 @@
 var totalExp = 0;
 var levelupExp = 300;
-for(var i = 0; i < 10; i++) { 
-    var exp = Math.floor(Math.random() * 51) + 30;
-	totalExp += exp;
-	console.log("勇者は" + (i + 1) + "回目のトレーニングで" + exp + "の経験値を得た");
-	if(levelupExp <= totalExp) {
-		console.log("勇者のレベルが上がりました");
-		levelupExp += 300;
-	}
-	else {
-		console.log("次のレベルまで" + (levelupExp - totalExp) + "の経験値が必要です")
-	}
+for (var i=0; i<10; i++) {
+  var exp = Math.floor(Math.random() * 51) +30;
+  totalExp += exp;
+  console.log("勇者は" + (i+1) + "回目のトレーニングで" + exp + "経験値を得た");
+  if (levelupExp <= totalExp) {
+    console.log("勇者のレベルが上がりました");
+    levelupExp += 300;
+  }
+  else{
+    console.log("次のレベルまで" + (levelupExp - totalExp) + "の経験値が必要です");
+  }
 }
+
+
+//勇者は
+//回目のトレーニングで
+//経験値を得た
+//勇者のレベルが上がりました
+//次のレベルまで
+//の経験値が必要です
